4c732ea5a9439cc54a96fa72c44f49c4b14e11cb
Sergey <ssotnikov1990@gmail.com>
2021-12-12 23:52:51 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
-  title = 'weather';
+  cityList = [];
+  choiceCity: any;
+  temperatureCelsius:any;
+  atmosphericPressure:any;
+  humidity:any;
+  windSpeed:any;
+  
+  constructor(private http: HttpClient) {}
+
+  showWeather(event: any) {
+    this.choiceCity = event.target.textContent;
+    console.log(this.choiceCity);
+    this.http
+    .get(
+      `http://api.airvisual.com/v2/city?city=${this.choiceCity}&state=California&country=USA&key=85b08754-d67d-492f-ab21-e4fc4718756b`
+    )
+    .subscribe((response: any) => {
+      console.log(response);
+      this.temperatureCelsius= response.data.current.weather.tp;
+      this.atmosphericPressure=response.data.current.weather.pr;
+      this.humidity=response.data.current.weather.hu;
+      this.windSpeed=response.data.current.weather.ws;
+    });
+  }
+
+  ngOnInit() {
+    this.http
+      .get(
+        'http://api.airvisual.com/v2/cities?state=California&country=USA&key=85b08754-d67d-492f-ab21-e4fc4718756b'
+      )
+      .subscribe((response: any) => {
+        this.cityList = response.data.map((item: any) => item.city);
+        /* console.log(this.cityList); */
+      });
+  }
 }
