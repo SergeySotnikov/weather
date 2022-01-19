33aa23f04a648edae39220c6a15fa7cb1be7ea60
Sergey Sotnikov <ssotnikov1990@gmail.com>
2022-01-19 10:48:22 +0300

    add switchMap

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { WeatherDataService } from '../services/weather-data.service';
+import { map, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.sass'],
 })
-
 export class HomeComponent implements OnInit {
-
   constructor(
     private http: HttpClient,
     private weatherDataService: WeatherDataService
@@ -35,8 +34,6 @@ export class HomeComponent implements OnInit {
     this.http.get(url).subscribe((response: any) => {
       this.stateList = response.data.map((item: any) => item.state);
     });
-
-   
   }
 
   getCityList(state: String) {
@@ -46,16 +43,26 @@ export class HomeComponent implements OnInit {
     const domain = 'http://api.airvisual.com';
     const endPointCity = `/v2/cities?state=${state}&country=${this.selectedCountry}`;
     const APIKey = '&key=85b08754-d67d-492f-ab21-e4fc4718756b';
+
     const url = `${domain}${endPointCity}${APIKey}`;
-    this.http.get(url).subscribe((response: any) => {
-      console.log(response);
-      this.cityList = response.data.map((item: any) => item.city);
-    });
+
+    this.http.get(url)
+      .pipe(
+        map((response: any) => {
+          this.cityList = response.data[0].city;
+        }),
+        switchMap(() =>
+          this.http.get(
+            `http://api.airvisual.com/v2/city?city=${this.cityList}&state=${this.selectedState}&country=${this.selectedCountry}&key=85b08754-d67d-492f-ab21-e4fc4718756b`
+          )))
+      .subscribe((response) => {
+        this.weatherDataService.showFirstCityWeather(response);
+      });
   }
 
-  getWeatherInfo(city: String) {
+  /*   getWeatherInfo(city: String) {
     this.weatherDataService.showWeatherSelect(this.selectedCountry,this.selectedState,city);
-  }
+  } */
 
   ngOnInit(): void {
     const domain = 'http://api.airvisual.com';
--- src/app/services/weather-data.service.ts
+++ src/app/services/weather-data.service.ts
@@ -14,7 +14,7 @@ export class WeatherDataService {
     atmosphericPressure: 0,
     humidity: 0,
     windSpeed: 0,
-    image: ''
+    image: '',
   };
   subject = new Subject<any>();
 
@@ -28,7 +28,7 @@ export class WeatherDataService {
     const endPointChoiceCities = `/v2/city?city=${this.cityName}&state=California&country=USA`;
     const APIKey = '&key=85b08754-d67d-492f-ab21-e4fc4718756b';
     const url = `${domain}${endPointChoiceCities}${APIKey}`;
-    
+
     this.http.get(url).subscribe((response: any) => {
       this.weatherInfo.cityName = response.data.city;
       this.weatherInfo.temperatureCelsius = response.data.current.weather.tp;
@@ -40,13 +40,12 @@ export class WeatherDataService {
     });
   }
 
-  showWeatherSelect(country:any, state:any,city:any) {
-
+  showWeatherSelect(country: any, state: any, city: any) {
     const domain = 'http://api.airvisual.com';
     const endPointChoiceCities = `/v2/city?city=${city}&state=${state}&country=${country}`;
     const APIKey = '&key=85b08754-d67d-492f-ab21-e4fc4718756b';
     const url = `${domain}${endPointChoiceCities}${APIKey}`;
-    
+
     this.http.get(url).subscribe((response: any) => {
       this.weatherInfo.cityName = response.data.city;
       this.weatherInfo.temperatureCelsius = response.data.current.weather.tp;
@@ -54,8 +53,19 @@ export class WeatherDataService {
       this.weatherInfo.humidity = response.data.current.weather.hu;
       this.weatherInfo.windSpeed = response.data.current.weather.ws;
       this.weatherInfo.image = response.data.current.weather.ic;
-      console.log(this.weatherInfo); 
+      console.log(this.weatherInfo);
       this.subject.next(this.weatherInfo);
     });
+  }
+
+  showFirstCityWeather(response: any) {
+    this.weatherInfo.cityName = response.data.city;
+    this.weatherInfo.temperatureCelsius = response.data.current.weather.tp;
+    this.weatherInfo.atmosphericPressure = response.data.current.weather.pr;
+    this.weatherInfo.humidity = response.data.current.weather.hu;
+    this.weatherInfo.windSpeed = response.data.current.weather.ws;
+    this.weatherInfo.image = response.data.current.weather.ic;
+    console.log(this.weatherInfo);
+    this.subject.next(this.weatherInfo);
   }
 }
